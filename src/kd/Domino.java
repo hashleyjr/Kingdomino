139358b660f1364828ef6886acedc9dbcb3d3037
Kevin <kchannan75@gmail.com>
2017-09-02 09:27:07 -0400

    Created Domino and Square classes

---
+++ src/kd/Domino.java
@@ -0,0 +1,12 @@
+package kd;
+
+public class Domino {
+	
+	private Square leftSquare;
+	private Square rightSquare;
+	
+	public Domino(Square leftSquare, Square rightSquare){
+		this.leftSquare = leftSquare;
+		this.rightSquare = rightSquare;
+	}
+}
---
+++ src/kd/Square.java
@@ -0,0 +1,12 @@
+package kd;
+
+public class Square {
+	private String terrainType;
+	private int crowns;
+	private String name;
+	
+	public Square(String terrainType, int crowns){
+		this.terrainType = terrainType;
+		this.crowns = crowns;
+	}
+}
