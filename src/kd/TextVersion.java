87dc107a4696031047e26bc1854e62dc5d22e0ce
Kevin <kchannan75@gmail.com>
2017-11-21 12:16:18 -0500

    Refactoring
    Created DominoPlacement class, which represents a placement of a domino
    (x,y, the domino toBePlaced, heading). All of the methods in TextVersion
    that were taking those four values as parameters, and now taking a
    DominoPlacement object instead. 
    Extracted out of the placeTheDomino method two new methods:
    specifyPlacement gathers the X,Y and heading values from the user into a
    DominoPlacement object, and makePlacement places the Domino according
    the the values of the DominoPlacement Object returned by
    specifyPlacement

--- src/kd/TextVersion.java
+++ src/kd/TextVersion.java
@@ -10,7 +10,9 @@ public class TextVersion {
 	Player thePlayer = new Player();
 	DominoSupply theSupply = new DominoSupply();
 	Scanner reader = new Scanner(System.in);
-	boolean isGameOver = false;
+	int playerCount;
+	int x, y;
+	DominoPlacement thePlacement;
 
 	public Domino giveRandomDomino() {
 		Domino theTurn = new Domino();
@@ -26,97 +28,114 @@ public class TextVersion {
 		return theTurn;
 	}
 
-	// This method gives the player a random domino, lets them specify an XY
-	// coordination and a heading, and checks if its a legal placement using the
-	// isntOccupied and hasMatchingTerrain methods.
-	public void printAllDominos() {
-		for (int i = 0; i <= theSupply.getUnplayed().length; i++) {
-			theSupply.getUnplayed()[48].printADomino();
+	public void setPlayerCount() {
+		System.out.println("How many players?");
+		playerCount = reader.nextInt();
+	}
+
+	public void doNewGame() {
+
+	}
+//This method takes the placement specifications for the chosen domino. Takes an X,Y coordinate, checks of that space isn't already occupied, takes
+	//the heading, and returns a DominoPlacement.
+	public DominoPlacement specifyPlacement(Domino toBePlaced) {
+		int x = 0, y = 0;
+
+		String heading;
+		boolean validXYChoice = false;
+		DominoPlacement thePlacement;
+		while (!validXYChoice) {
+			System.out.println("Specify an X coord");
+			x = reader.nextInt();
+			System.out.println("Specify a Y coord");
+			y = reader.nextInt();
+			if (isntOccupied(x, y)) {
+				System.out.println("You picked a valid XY location");
+				validXYChoice = true;
+			} else {
+				System.out.println("This spot is already occupied, try again");
+			}
+		}
+		System.out.println("Specify the heading");
+		heading = reader.next();
+		thePlacement = new DominoPlacement(x, y, toBePlaced, heading);
+		return thePlacement;
+
+	}
+
+	//this method places the chosen Domino "toBePlaced" at the specified XY location, facing the direction of the heading. It does not 
+	//check if it is a legal placement.
+	public void makePlacement(DominoPlacement thePlacement) {
+		int x = thePlacement.x;
+		int y = thePlacement.y;
+		Domino toBePlaced = thePlacement.toBePlaced;
+		String heading = thePlacement.heading;
+		switch (heading) {
+
+		case ("S"):
+
+			thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
+			thePlayer.getPlayerBoard()[x][y - 1] = toBePlaced.getLeft();
+
+			break;
+
+		case ("N"):
+			thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
+			thePlayer.getPlayerBoard()[x][y + 1] = toBePlaced.getLeft();
+
+			break;
+
+		case ("E"):
+
+			thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
+			thePlayer.getPlayerBoard()[x - 1][y] = toBePlaced.getLeft();
+
+			break;
+		case ("W"):
+
+			thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
+			thePlayer.getPlayerBoard()[x + 1][y] = toBePlaced.getLeft();
+
+			break;
+
 		}
 	}
 
+	// This method gives the player a random domino, lets them specify an XY
+	// coordination and a heading, and checks if its a legal placement using the
+	// isntOccupied and hasMatchingAdjacentTerrain methods.
 	public void placeTheDomino() {
 
 		Domino toBePlaced;
 
-		int x = 0;
-		int y = 0;
 		String heading;
 		boolean validXYChoice;
 		boolean validChoice;
-
+		DominoPlacement currentPlacement;
 		for (int i = 0; i < 12;) {
 			thePlayer.setBoardBounds();
 			thePlayer.printBoard();
 			toBePlaced = giveRandomDomino();
+
 			validChoice = false;
 			while (!validChoice) {
-				validXYChoice = false;
-				while (!validXYChoice) {
-					toBePlaced.printADomino();
-					System.out.println("Please place this domino, specify x coord");
-					x = reader.nextInt();
-					System.out.println("Specify y coord");
-					y = reader.nextInt();
-					if (isntOccupied(x, y)) {
-						System.out.println("You picked a valid XY location");
-						validXYChoice = true;
-					} else {
-						System.out.println("This spot is already occupied, try again");
-					}
-				}
-				System.out.println("Specify the heading");
-				heading = reader.next();
-				switch (heading) {
+				toBePlaced.printADomino();
+				currentPlacement = specifyPlacement(toBePlaced);
 
-				case ("S"):
-
-					if (isLegalPlacement(x, y, toBePlaced, heading)) {
-
-						thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
-						thePlayer.getPlayerBoard()[x][y - 1] = toBePlaced.getLeft();
-						validChoice = true;
-						i++;
-					} else {
-						System.out.println("This is not a legal placement.");
-
-					}
-					break;
-				case ("N"):
-					if (isLegalPlacement(x, y, toBePlaced, heading)) {
-						thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
-						thePlayer.getPlayerBoard()[x][y + 1] = toBePlaced.getLeft();
-						validChoice = true;
-						i++;
-					} else {
-						System.out.println("This is not a legal placement.");
-					}
-					break;
-				case ("E"):
-					if (isLegalPlacement(x, y, toBePlaced, heading)) {
-						thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
-						thePlayer.getPlayerBoard()[x - 1][y] = toBePlaced.getLeft();
-						validChoice = true;
-						i++;
-					} else {
-						System.out.println("This is not a legal placement.");
-					}
-					break;
-				case ("W"):
-					if (isLegalPlacement(x, y, toBePlaced, heading)) {
-						thePlayer.getPlayerBoard()[x][y] = toBePlaced.getPivot();
-						thePlayer.getPlayerBoard()[x + 1][y] = toBePlaced.getLeft();
-						validChoice = true;
-						i++;
-					} else {
-						System.out.println("This is not a legal placement.");
-					}
-					break;
-				case ("U"):
+				if (currentPlacement.heading.equals("U")) {
 					System.out.print("Too bad");
 					validChoice = true;
 					i++;
-					break;
+				} else {
+					if (isLegalPlacement(currentPlacement)) {
+
+						makePlacement(currentPlacement);
+						validChoice = true;
+						i++;
+					} else {
+						System.out.println("This is not a legal placement.");
+
+					}
 				}
 				thePlayer.printBoard();
 			}
@@ -125,30 +144,33 @@ public class TextVersion {
 
 	}
 
-	// This method uses the hasMatchingTerrain method to check if the tile being
+	// This method uses the hasMatchingAdjacentTerrain method to check if the tile being
 	// placed has adjacent matching terrain, and if the tile isn't overlapping
 	// with an already placed tile
-	public boolean isLegalPlacement(int x, int y, Domino toBePlaced, String heading) {
+	public boolean isLegalPlacement(DominoPlacement thePlacement) {
+		int x = thePlacement.x;
+		int y = thePlacement.y;
+		String heading = thePlacement.heading;
 		switch (heading) {
 		case "S":
-			if (hasMatchingTerrain(x, y, toBePlaced, heading) && isntOccupied(x, y - 1))
+			if (hasMatchingAdjacentTerrain(thePlacement) && isntOccupied(x, y - 1))
 				return true;
 			else {
 
 				return false;
 			}
 		case "N":
-			if (hasMatchingTerrain(x, y, toBePlaced, heading) && isntOccupied(x, y + 1))
+			if (hasMatchingAdjacentTerrain(thePlacement) && isntOccupied(x, y + 1))
 				return true;
 			else
 				return false;
 		case "E":
-			if (hasMatchingTerrain(x, y, toBePlaced, heading) && isntOccupied(x - 1, y))
+			if (hasMatchingAdjacentTerrain(thePlacement) && isntOccupied(x - 1, y))
 				return true;
 			else
 				return false;
 		case "W":
-			if (hasMatchingTerrain(x, y, toBePlaced, heading) && isntOccupied(x + 1, y))
+			if (hasMatchingAdjacentTerrain(thePlacement) && isntOccupied(x + 1, y))
 				return true;
 			else
 				return false;
@@ -175,21 +197,24 @@ public class TextVersion {
 	// In a game of KingDomino, a tile being laid has to have have at least one
 	// of its terrain types matching the terrain type of a tile
 	// adjacent to where it's being laid. This method checks for that.
-	public boolean hasMatchingTerrain(int x, int y, Domino d, String heading) {
-
+	public boolean hasMatchingAdjacentTerrain(DominoPlacement thePlacement) {
+		int x = thePlacement.x;
+		int y = thePlacement.y;
+		Domino toBePlaced = thePlacement.toBePlaced;
+		String heading = thePlacement.heading;
 		switch (heading) {
 		case "S":
 
 			for (int i = -1; i <= 1; i += 2) {
 				if (thePlayer.getPlayerBoard()[x + i][y].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + i][y].getName() == d.getPivot().getName()
+						|| thePlayer.getPlayerBoard()[x + i][y].getName() == toBePlaced.getPivot().getName()
 						|| thePlayer.getPlayerBoard()[x][y + 1].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x][y + 1].getName() == d.getPivot().getName()) {
+						|| thePlayer.getPlayerBoard()[x][y + 1].getName() == toBePlaced.getPivot().getName()) {
 					return true;
 				} else if (thePlayer.getPlayerBoard()[x + i][y - 1].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + i][y - 1].getName() == d.getLeft().getName()
+						|| thePlayer.getPlayerBoard()[x + i][y - 1].getName() == toBePlaced.getLeft().getName()
 						|| thePlayer.getPlayerBoard()[x][y - 2].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x][y - 2].getName() == d.getLeft().getName()) {
+						|| thePlayer.getPlayerBoard()[x][y - 2].getName() == toBePlaced.getLeft().getName()) {
 					return true;
 				}
 
@@ -201,14 +226,14 @@ public class TextVersion {
 
 			for (int i = -1; i <= 1; i += 2) {
 				if (thePlayer.getPlayerBoard()[x + i][y].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + i][y].getName() == d.getPivot().getName()
+						|| thePlayer.getPlayerBoard()[x + i][y].getName() == toBePlaced.getPivot().getName()
 						|| thePlayer.getPlayerBoard()[x][y - 1].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x][y - 1].getName() == d.getPivot().getName()) {
+						|| thePlayer.getPlayerBoard()[x][y - 1].getName() == toBePlaced.getPivot().getName()) {
 					return true;
 				} else if (thePlayer.getPlayerBoard()[x + i][y + 1].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + i][y + 1].getName() == d.getLeft().getName()
+						|| thePlayer.getPlayerBoard()[x + i][y + 1].getName() == toBePlaced.getLeft().getName()
 						|| thePlayer.getPlayerBoard()[x][y + 2].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x][y + 2].getName() == d.getLeft().getName()) {
+						|| thePlayer.getPlayerBoard()[x][y + 2].getName() == toBePlaced.getLeft().getName()) {
 					return true;
 				}
 
@@ -220,14 +245,14 @@ public class TextVersion {
 
 			for (int i = -1; i <= 1; i += 2) {
 				if (thePlayer.getPlayerBoard()[x][y + i].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x][y + i].getName() == d.getPivot().getName()
+						|| thePlayer.getPlayerBoard()[x][y + i].getName() == toBePlaced.getPivot().getName()
 						|| thePlayer.getPlayerBoard()[x + 1][y].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + 1][y].getName() == d.getPivot().getName()) {
+						|| thePlayer.getPlayerBoard()[x + 1][y].getName() == toBePlaced.getPivot().getName()) {
 					return true;
 				} else if (thePlayer.getPlayerBoard()[x - 1][y + i].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x - 1][y + i].getName() == d.getLeft().getName()
+						|| thePlayer.getPlayerBoard()[x - 1][y + i].getName() == toBePlaced.getLeft().getName()
 						|| thePlayer.getPlayerBoard()[x - 2][y].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x - 2][y].getName() == d.getLeft().getName()) {
+						|| thePlayer.getPlayerBoard()[x - 2][y].getName() == toBePlaced.getLeft().getName()) {
 					return true;
 				}
 
@@ -239,14 +264,14 @@ public class TextVersion {
 
 			for (int i = -1; i <= 1; i += 2) {
 				if (thePlayer.getPlayerBoard()[x][y + i].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x][y + i].getName() == d.getPivot().getName()
+						|| thePlayer.getPlayerBoard()[x][y + i].getName() == toBePlaced.getPivot().getName()
 						|| thePlayer.getPlayerBoard()[x - 1][y].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x - 1][y].getName() == d.getPivot().getName()) {
+						|| thePlayer.getPlayerBoard()[x - 1][y].getName() == toBePlaced.getPivot().getName()) {
 					return true;
 				} else if (thePlayer.getPlayerBoard()[x + 1][y + i].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + 1][y + i].getName() == d.getLeft().getName()
+						|| thePlayer.getPlayerBoard()[x + 1][y + i].getName() == toBePlaced.getLeft().getName()
 						|| thePlayer.getPlayerBoard()[x + 2][y].getName() == "C"
-						|| thePlayer.getPlayerBoard()[x + 2][y].getName() == d.getLeft().getName()) {
+						|| thePlayer.getPlayerBoard()[x + 2][y].getName() == toBePlaced.getLeft().getName()) {
 					return true;
 				}
 
